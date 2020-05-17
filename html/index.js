1f8c71453374d7d6d24adbb2c4c676a7db543cfe
stormmurdoc <murdoc@storm-clan.de>
2020-05-17 08:58:19 +0200

    initial checkin

---
+++ html/index.js
@@ -0,0 +1,97 @@
+$(document).ready(function () {
+  var bubbleChart = new d3.svg.BubbleChart({
+    supportResponsive: true,
+    //container: => use @default
+    size: 600,
+    //viewBoxSize: => use @default
+    innerRadius: 600 / 3.5,
+    //outerRadius: => use @default
+    radiusMin: 50,
+    //radiusMax: use @default
+    //intersectDelta: use @default
+    //intersectInc: use @default
+    //circleColor: use @default
+    data: {
+      items: [
+        {text: "Ergebnisorientierung", count: "100"},
+        {text: "Empathie", count: "10"},
+        {text: "Durchsetzungsfähigkeit", count: "50"},
+        {text: "Php", count: "10"},
+        {text: "Ruby", count: "10"},
+        {text: "D", count: "20"},
+        {text: "Python", count: "30"},
+        {text: "C/C++", count: "2"},
+        {text: "Pascal", count: "10"},
+        {text: "Something", count: "10"},
+      ],
+      eval: function (item) {return item.count;},
+      classed: function (item) {return item.text.split(" ").join("");}
+    },
+    plugins: [
+      {
+        name: "central-click",
+        options: {
+          text: "(See more detail)",
+          style: {
+            "font-size": "12px",
+            "font-style": "italic",
+            "font-family": "Source Sans Pro, sans-serif",
+            //"font-weight": "700",
+            "text-anchor": "middle",
+            "fill": "white"
+          },
+          attr: {dy: "65px"},
+          centralClick: function() {
+            alert("Here is more details!!");
+          }
+        }
+      },
+      {
+        name: "lines",
+        options: {
+          format: [
+            {// Line #0
+              textField: "text",
+              classed: {text: true},
+              style: {
+                "font-size": "14px",
+                "font-family": "Source Sans Pro, sans-serif",
+                "text-anchor": "middle",
+                fill: "white"
+              },
+              attr: {
+                dy: "20px",
+                x: function (d) {return d.cx;},
+                y: function (d) {return d.cy;}
+              }
+            },
+            {// Line #1
+              textField: "count",
+              classed: {count: true},
+              style: {
+                "font-size": "4px",
+                "font-family": "Source Sans Pro, sans-serif",
+                "text-anchor": "middle",
+                fill: "white"
+              },
+              attr: {
+                dy: "0px",
+                x: function (d) {return d.cx;},
+                y: function (d) {return d.cy;}
+              }
+            }
+          ],
+          centralFormat: [
+            {// Line #0
+              style: {"font-size": "50px"},
+              attr: {}
+            },
+            {// Line #1
+              style: {"font-size": "20px"},
+              attr: {dy: "40px"}
+            }
+          ]
+        }
+      }]
+  });
+});
